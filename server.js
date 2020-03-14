5f1c44029a01906ead098087496fdc9fba89d9ac
danh9a99 <57696188+danh9a99@users.noreply.github.com>
2020-03-14 10:26:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // # SimpleServer
 // A simple chat bot server
-var logger = require('morgan');
 var http = require('http');
 var bodyParser = require('body-parser');
 var express = require('express');
-var router = express();
 
 var app = express();
-app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 var server = http.createServer(app);
 var request = require("request");
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
 
 app.get('/', (req, res) => {
   res.send("Home page. Server running okay.");
